c28cb7f295ad3fe38c99c6beda822f2603e39724
azuretran <99276011+linhtran-281098@users.noreply.github.com>
2022-11-29 08:59:39 +0700

    x

--- trading_nft_quanglinh/src/components/Transactions.jsx
+++ trading_nft_quanglinh/src/components/Transactions.jsx
@@ -21,7 +21,7 @@ const Transactions = () => {
     <div className="bg-[#151c25]">
       <div className="w-4/5 py-10 mx-auto">
         <h4 className="text-white text-3xl font-bold uppercase text-gradient">
-          {collection.length > 0 ? 'Latest Transactions' : 'No Transaction Yet'}
+          {collection.length > 0 ? 'CÁC GIAO DỊCH MỚI NHẤT' : 'CHƯA CÓ GIAO DỊCH'}
         </h4>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-4 lg:gap-2 py-2.5">
